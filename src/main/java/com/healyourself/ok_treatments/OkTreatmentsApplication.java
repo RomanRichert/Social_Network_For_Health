450b62c9ac89a8e0969ed195c19abc323ef3bbf0
Roman Richert <roman_richert@vk.com>
2023-03-20 19:13:24 +0100

    Initial commit.

---
+++ src/main/java/com/healyourself/ok_treatments/OkTreatmentsApplication.java
@@ -0,0 +1,13 @@
+package com.healyourself.ok_treatments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkTreatmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkTreatmentsApplication.class, args);
+	}
+
+}
