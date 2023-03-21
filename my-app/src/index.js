3d7351fe3bbc5c094c8cd7c8c943cc59c7bbbd32
Evgeny <bril424@mail.ru>
2023-03-21 14:41:39 +0100

    store created

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from "./store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <App />
+  <Provider store={ store }>
+    <HashRouter>
+      <App />
     </HashRouter>
+  </Provider>
+  
 );
 
