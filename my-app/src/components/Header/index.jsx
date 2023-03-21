6c59a0253fa9881b8d238ef88b29917730dca06f
mariam-85 <mariam.lofink85@gmail.com>
2023-03-21 10:53:12 +0100

    header and main style added

---
+++ my-app/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.css'
+import logo from './media/logo.png'
+
+export default function Header() {
+
+  return (
+        <div className={styles.logo}>
+                <img src={logo} alt="logo" />
+        </div>
+  )
+}
