68479afcadf16515469cead99b1678b0bf12667a
callbackKid <vladislav.yarosh23@gmail.com>
2023-03-22 00:29:30 +0400

    button fix

--- my-app/src/components/QuestionItem/index.jsx
+++ my-app/src/components/QuestionItem/index.jsx
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 export default function QuestionItem({ id, question, answers }) {
+  const [selectedValue, setSelectedValue] = useState("");
 
-    const [selectedValue, setSelectedValue] = useState('');
-  
-    const handleOptionChange = (event) => {
-      // setSelectedValue(selectedValue); 
-      event.preventDefault()
-      const { question } = event.target;
-      console.log(question.value);
-
-    }
+  const handleOptionChange = (event) => {
+    console.log(event.target.value);
+    //setSelectedValue(selectedValue);
+    setSelectedValue(event.target.value);
+    // const { question } = event.target;
+    // console.log(question.value);
+  };
 
   return (
-	
-        <div>
-          <legend>{question}</legend>
-          <form onSubmit={() => handleOptionChange}>
-          {answers.map((answer) => (
-            <fieldset key={answer.id}>
-            <input type="radio" name='question' value={answer.id} />
-              <label onClick={console.log(answer.id, answer.answer)}>
-                {answer.answer}
-              </label>
-            </fieldset>
-          ))}
-          </form>
-        </div>        
-      )
-    };
-    
-  
-
+    <div>
+      <legend>{question}</legend>
+      <form>
+        {answers.map((answer) => (
+          <fieldset key={answer.id}>
+            <input
+              type="radio"
+              name="question"
+              value={answer.id}
+              onClick={handleOptionChange}
+            />
+            <label>{answer.answer}</label>
+          </fieldset>
+        ))}
+      </form>
+    </div>
+  );
+}
