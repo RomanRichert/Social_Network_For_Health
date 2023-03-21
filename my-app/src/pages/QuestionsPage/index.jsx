1a1c34841b7ca53d1bb461568ceddb09b5019321
mariam-85 <mariam.lofink85@gmail.com>
2023-03-21 21:55:12 +0100

    radio buttons added

--- my-app/src/pages/QuestionsPage/index.jsx
+++ my-app/src/pages/QuestionsPage/index.jsx
@@ -3,7 +3,6 @@ import QuestionItem from "../../components/QuestionItem";
 import { questions } from '../../data'
 import Button from '../../components/Button'
 import { useParams } from 'react-router-dom'
-import style from './index.module.css'
 
 
 export default function QuestionsPage() {
