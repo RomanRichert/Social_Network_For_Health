6103c1625d2dd904ad58cbbe89083b3fe7d9d5da
mariam-85 <mariam.lofink85@gmail.com>
2023-03-21 22:19:31 +0100

    styles added

--- my-app/src/components/QuestionItem/index.jsx
+++ my-app/src/components/QuestionItem/index.jsx
@@ -15,16 +15,16 @@ export default function QuestionItem({ id, question, answers }) {
   };
 
   return (
-    <div>
-      <legend>{question}</legend>
+    <div className={style.question_item}>
+      <h2>{question}</h2>
       <form>
         {answers.map((answer) => (
-          <fieldset
+          <fieldset  className={style.form}
             key={answer.id}
             name={answer.id}
             onClick={handleOptionChange}
           >
-            <input
+            <input className={style.radio}
               type="radio"
               name="question"
               value={answer.id}
