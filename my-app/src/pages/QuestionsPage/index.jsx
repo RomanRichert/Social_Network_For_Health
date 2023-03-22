02bda01b93d86e3e8227c76139eb81ac998f29c3
Evgeny <bril424@mail.ru>
2023-03-22 10:45:05 +0100

    SubmitPage added

--- my-app/src/pages/QuestionsPage/index.jsx
+++ my-app/src/pages/QuestionsPage/index.jsx
@@ -2,34 +2,70 @@ import React from "react";
 import QuestionItem from "../../components/QuestionItem";
 import { questions } from "../../data";
 import Button from "../../components/Button";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate, Link } from "react-router-dom";
 import style from "./index.module.css";
-import { Link } from "react-router-dom";
+import ResultsPage from "../ResultsPage";
+import FirstPage from "../FirstPage";
 
 export default function QuestionsPage() {
   const { id } = useParams();
 
+  //   if (+id === 1) {
+  //     <Navigate to={<FirstPage />} />;
+  //   }
+  //   if (+id === 35) {
+  //     <Navigate to={<ResultsPage />} />;
+  //   }
+
   return (
     <div className={style.questions_page}>
-      {<QuestionItem {...questions[+id]} />}
+      {<QuestionItem {...questions[+id - 1]} />}
       {+id === 1 ? (
-        <Link to={"/"} className={style.btn_block}>
-          <Button>back</Button>
-        </Link>
+        <>
+          <Link to={"/"} className={style.btn_block}>
+            <Button>back</Button>
+          </Link>
+          <Link to={`/${+id + 1}`} className={style.btn_block}>
+            <Button>next</Button>
+          </Link>
+        </>
       ) : (
-        <Link to={`/${+id - 1}`} className={style.btn_block}>
-          <Button>next</Button>
-        </Link>
+        <>
+          <Link to={`/${+id - 1}`} className={style.btn_block}>
+            <Button>back</Button>
+          </Link>
+
+          <Link to={`/${+id + 1}`} className={style.btn_block}>
+            <Button>next</Button>
+          </Link>
+        </>
       )}
-      {/* {+id === 36 ? (
-        <Link to={"/"} className={style.btn_block}>
+      {/* if ( +id === 36 ) */}
+      {
+        <Link to={"results"} className={style.btn_block}>
           <Button>next</Button>
         </Link>
+      }
+      {+id === 36 ? (
+        <>
+          <Link to={"results"} className={style.btn_block}>
+            <Button>next</Button>
+          </Link>
+          <Link to={`/${+id - 1}`} className={style.btn_block}>
+            <Button>back</Button>
+          </Link>
+        </>
       ) : (
-        <Link to={`/${+id + 1}`} className={style.btn_block}>
-          <Button>back</Button>
-        </Link>
-      )} */}
+        <>
+          <Link to={`/${+id - 1}`} className={style.btn_block}>
+            <Button>back</Button>
+          </Link>
+
+          <Link to={`/${+id + 1}`} className={style.btn_block}>
+            <Button>next</Button>
+          </Link>
+        </>
+      )}
     </div>
   );
 }
---
+++ my-app/src/pages/SubmitPage/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import style from "./index.module.css";
+import Icon1 from "./media/anguished_icon.svg";
+import Icon2 from "./media/unamused_icon.svg";
+import Icon3 from "./media/neutral_icon.svg";
+import Icon4 from "./media/smilling_icon.svg";
+import Icon5 from "./media/beaming_icon.svg";
+
+export default function SubmitPage() {
+  return (
+    <div>
+      <div className={style.title_block}>
+        <p>Height</p>
+        <p>Weight</p>
+        <p>Age</p>
+      </div>
+      <div className={style.submit_block}>
+        <input type="text" name="height" />
+        <input type="text" name="weight" />
+        <input type="number" name="age" />
+      </div>
+      <p className={style.title}>Therapy</p>
+      <div className={style.title2_block}>
+        <select>
+          <option>Running</option>
+          <option>Yoga</option>
+          <option>Swim</option>
+        </select>
+        <div className={style.img_block}>
+          <img src={Icon1} alt="" />
+          <img src={Icon2} alt="" />
+          <img src={Icon3} alt="" />
+          <img src={Icon4} alt="" />
+          <img src={Icon5} alt="" />
+        </div>
+      </div>
+
+      <div className={style.story_block}>
+        <textarea name="story" cols="30" rows="10" maxLength="250"></textarea>
+        <button>Publish my story</button>
+      </div>
+    </div>
+  );
+}
