40db35c826d01e07a8a987f4236c2fe73dd83652
Karen <motorolochka@mail.ru>
2023-03-24 13:39:54 +0100

    got the answers

--- my-app/src/pages/ResultsPage/index.jsx
+++ my-app/src/pages/ResultsPage/index.jsx
@@ -5,13 +5,10 @@ import { getAllStories } from "../../requests/getAllStoriesRequest";
 import styles from "./index.module.css";
 import Img from "./media/BMI.jpg";
 import { Link } from 'react-router-dom'
-import {HeartOutlined} from '@ant-design/icons'
+import { HeartOutlined } from '@ant-design/icons'
 
 export default function ResultsPage() {
   const [text, setText] = useState([]);
-
-  const dispatch = useDispatch()
-//   const stories = useSelector((state) => state.stories);
 
   const allAnswers = useSelector(state => state.answers)
   console.log('results', allAnswers)
@@ -27,16 +24,17 @@ export default function ResultsPage() {
     message.value = "";
   };
 
+  const [stories, setStories] = useState([])
   useEffect(() => {
     const getStory = async () => {
       const response = await fetch("http://localhost:8080/story");
-      const stories = await response.json();
-    	console.log(stories, "sfsff");
+      const storiesResponse = await response.json();
+	  setStories(storiesResponse)
+  
     };
     getStory();
-	// dispatch(getAllStories)
   }, []);
-
+  console.log(stories.slice(0, 2), "stories");
   return (
     <div className={styles.results_page}>
 		{
@@ -52,36 +50,34 @@ export default function ResultsPage() {
       <p>Meet your treatment buddies:</p>
 
       <div className={styles.treatments}>
-		{/* {
-			// allAnswers && stories.map(el => <p>{el}</p>)
-		} */}
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-          quaerat, magnam quibusdam id molestias quas sint atque illum dolore
-          dolor.
-        </p>
-        <form onSubmit={submit}>
-          <textarea
-            name="message"
-            cols="30"
-            rows="10"
-            maxLength="250"
-            placeholder="*The commentary must contain no more than 250 characters"
-          ></textarea>
-          <div className={styles.message}>
-            {text.map((el, index) => (
-				<>
-				    <p key={index}>{el}&nbsp;&nbsp;
-			  		<HeartOutlined /> </p>
-				</>
+		{
+				stories && stories.slice(0, 2).map(e => <>
+					<p>{e.description}</p>
+					<form onSubmit={submit}>
+					<textarea
+						name="message"
+						cols="30"
+						rows="10"
+						maxLength="250"
+						placeholder="*The commentary must contain no more than 250 characters"
+					></textarea>
+					<div className={styles.message}>
+						{text.map((el, index) => (
+							<>
+								<p key={index}>{el}&nbsp;&nbsp;
+								<HeartOutlined /> </p>
+							</>
 
-            ))}
-          </div>
-          <div className={styles.actions_btns}>
-            <button>Comment</button>
-            <div>I feel sorry for you</div>
-          </div>
-        </form>
+						))}
+					</div>
+					<div className={styles.actions_btns}>
+						<button>Comment</button>
+						<div>I feel sorry for you</div>
+					</div>
+        		</form>
+				</>)
+		}
+
       </div>
       <Link to='/'>
             <button className={styles.back_button}>back to start</button>
