430e1ac92f4d6fff17ae2eb3b890f9636316f8b2
Roman Richert <roman_richert@vk.com>
2023-03-24 14:09:42 +0100

    Revert "tests"
    This reverts commit 038471f5b6a8ddd4680af6149715242c12ad475b.

--- src/test/java/com/healyourself/ok_treatments/controller/TherapyControllerTest.java
+++ src/test/java/com/healyourself/ok_treatments/controller/TherapyControllerTest.java
@@ -1,20 +1,10 @@
 package com.healyourself.ok_treatments.controller;
 
-import com.healyourself.ok_treatments.service.TherapyService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-@AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
-@WebMvcTest(StoryController.class)
+import static org.junit.jupiter.api.Assertions.*;
+
 class TherapyControllerTest {
-
-    @Mock // заглушаем работу с базой данных
-    private TherapyService therapyService;
 
     @Test
     void createTherapy() {
