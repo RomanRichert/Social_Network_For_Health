038471f5b6a8ddd4680af6149715242c12ad475b
Dmitry@HP-Spectre-360-Dim <IT-Dim@web.de>
2023-03-24 06:11:49 +0100

    tests

--- src/test/java/com/healyourself/ok_treatments/controller/TherapyControllerTest.java
+++ src/test/java/com/healyourself/ok_treatments/controller/TherapyControllerTest.java
@@ -1,10 +1,20 @@
 package com.healyourself.ok_treatments.controller;
 
+import com.healyourself.ok_treatments.service.TherapyService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(StoryController.class)
 class TherapyControllerTest {
+
+    @Mock // заглушаем работу с базой данных
+    private TherapyService therapyService;
 
     @Test
     void createTherapy() {
--- src/test/java/com/healyourself/ok_treatments/service/implementation/StoryServiceImplTest.java
+++ src/test/java/com/healyourself/ok_treatments/service/implementation/StoryServiceImplTest.java
@@ -1,16 +1,14 @@
 package com.healyourself.ok_treatments.service.implementation;
 
 import com.healyourself.ok_treatments.converter.JSONConverter;
-import com.healyourself.ok_treatments.mapper.StoryMapper;
-import com.healyourself.ok_treatments.mapper.StoryMapperImpl;
 import com.healyourself.ok_treatments.repository.StoryRepository;
 import com.healyourself.ok_treatments.repository.StoryVoteRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class StoryServiceImplTest {
@@ -18,12 +16,15 @@ class StoryServiceImplTest {
     @Mock
     private StoryRepository storyRepository;
 
-    private StoryMapper storyMapper = new StoryMapperImpl();
+    @Spy
+//    private StoryMapper storyMapper = new StoryMapperImpl();
+
 
     private JSONConverter converter;
 
     private StoryVoteRepository voteRepository;
 
+    @InjectMocks
     private StoryServiceImpl storyService;
 
     @Test
